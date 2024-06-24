31e364060a613fb8e0365c52948f9b58349a618c
Primeshheshan <primeshheshan@gmail.com>
2024-06-24 15:59:59 +0530

    feat: book images

---
+++ src/assets/images/books/index.ts
@@ -0,0 +1,7 @@
+import book1 from "./book1.png";
+import book2 from "./book2.png";
+import book3 from "./book3.png";
+import book4 from "./book4.png";
+import book5 from "./book5.png";
+
+export { book1, book2, book3, book4, book5 };
