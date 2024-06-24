71de0ce82afdc783d9cad9b09af525ff3d846470
Primeshheshan <primeshheshan@gmail.com>
2024-06-24 15:59:32 +0530

    feat: authors images

---
+++ src/assets/images/authors/index.ts
@@ -0,0 +1,8 @@
+import author1 from "./author1.png";
+import author2 from "./author2.png";
+import author3 from "./author3.png";
+import author4 from "./author4.png";
+import author5 from "./author5.png";
+import author6 from "./author6.png";
+
+export { author1, author2, author3, author4, author5, author6 };
